0239f40486bbade47526a59aa64437732ca22381
Saki <tomguts@126.com>
2014-05-15 16:16:07 +0800

    Major: 0.0.125
    New Features:
    1. KNLibHashPixmapList add new class for cache.

--- src/Modules/Base/knlibhashpixmaplist.cpp
+++ src/Modules/Base/knlibhashpixmaplist.cpp
@@ -1,14 +1,30 @@
+#include <QCryptographicHash>
 #include <QBuffer>
 
 #include "knlibhashpixmaplist.h"
 
-KNLibHashPixmapList::KNLibHashPixmapList(QObject *parent) :
+KNLibPixmapBuffer::KNLibPixmapBuffer(QObject *parent) :
     QObject(parent)
 {
     m_buffer=new QBuffer(&m_pixmapCache, this);
 }
 
+void KNLibPixmapBuffer::cachePixmap(const QPixmap &pixmap)
+{
+    pixmap.save(m_buffer, "png");
+}
+
+KNLibHashPixmapList::KNLibHashPixmapList(QObject *parent) :
+    QObject(parent)
+{
+}
+
 QPixmap KNLibHashPixmapList::pixmap(const QByteArray &array) const
 {
     return m_list[array];
+}
+
+QByteArray KNLibHashPixmapList::append(const QPixmap pixmap)
+{
+    ;
 }
--- src/Modules/Base/knlibhashpixmaplist.h
+++ src/Modules/Base/knlibhashpixmaplist.h
@@ -7,13 +7,30 @@
 #include <QObject>
 
 class QBuffer;
+
+class KNLibPixmapBuffer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit KNLibPixmapBuffer(QObject *parent = 0);
+    void cachePixmap(const QPixmap &pixmap);
+
+signals:
+    void cacheComplete();
+    void writeComplete();
+
+private:
+    QByteArray m_pixmapCache, m_hashCache;
+    QBuffer *m_buffer;
+};
+
 class KNLibHashPixmapList : public QObject
 {
     Q_OBJECT
 public:
     explicit KNLibHashPixmapList(QObject *parent = 0);
     QPixmap pixmap(const QByteArray &array) const;
-    ;
+    QByteArray append(const QPixmap pixmap);
 
 signals:
 
@@ -21,8 +38,6 @@ public slots:
 
 private:
     QHash<QByteArray, QPixmap> m_list;
-    QByteArray m_pixmapCache;
-    QBuffer *m_buffer;
 };
 
 #endif // KNLIBHASHPIXMAPLIST_H
