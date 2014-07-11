0d08ecfb592171ba549c0160485524d0367673af
Saki <tomguts@126.com>
2014-07-12 00:19:51 +0800

    Major: 0.0.387
    New Features:
    1. Button tweak and fixed the #11 bug.

--- src/Modules/Base/knstdlibviewercategorybutton.cpp
+++ src/Modules/Base/knstdlibviewercategorybutton.cpp
@@ -1,7 +1,9 @@
 #include <QEvent>
+#include <QPainter>
 #include <QMouseEvent>
 #include <QPaintEvent>
 #include <QResizeEvent>
+#include <QTimeLine>
 
 #include <QDebug>
 
@@ -13,19 +15,55 @@ KNStdLibViewerCategoryButton::KNStdLibViewerCategoryButton(QWidget *parent) :
     //Set properties.
     setAutoFillBackground(true);
     setContentsMargins(0,0,0,0);
-    setFixedHeight(30);
+    setFixedHeight(m_fixedHeight);
 
-    //Get palette.
-    m_palette=palette();
-    m_palette.setColor(QPalette::Window, m_backcolor);
-    setPalette(m_palette);
+    //Set default checked.
+    m_checked=false;
+    //Set icon position.
+    m_iconCenterX=(width()-m_iconSize)>>1;
+    //Set background gradient.
+    m_background=QLinearGradient(0,0,0,height());
+    m_background.setColorAt(0, m_gradientColor);
+    m_background.setColorAt(1, m_gradientColor);
 
-    ;
+    //Initial the mouse-in animation.
+    m_mouseIn=new QTimeLine(200, this);
+    m_mouseIn->setUpdateInterval(2);
+    m_mouseIn->setEndFrame(100);
+    m_mouseIn->setEasingCurve(QEasingCurve::OutCubic);
+    connect(m_mouseIn, &QTimeLine::frameChanged,
+            this, &KNStdLibViewerCategoryButton::onActionMouseInOutFrameChange);
+
+    //Initial the mouse-out animation.
+    m_mouseOut=new QTimeLine(200, this);
+    m_mouseOut->setUpdateInterval(2);
+    m_mouseOut->setEndFrame(0);
+    m_mouseOut->setEasingCurve(QEasingCurve::OutCubic);
+    connect(m_mouseOut, &QTimeLine::frameChanged,
+            this, &KNStdLibViewerCategoryButton::onActionMouseInOutFrameChange);
+
+    //Initial the mouse-down animation.
+    m_mouseDown=new QTimeLine(200, this);
+    m_mouseDown->setUpdateInterval(2);
+    m_mouseDown->setEndFrame(200);
+    m_mouseDown->setEasingCurve(QEasingCurve::OutCubic);
+    connect(m_mouseDown, &QTimeLine::frameChanged,
+            this, &KNStdLibViewerCategoryButton::onActionMousePressedChange);
+
+    //Initial the mouse-up animation.
+    m_mouseUp=new QTimeLine(200, this);
+    m_mouseUp->setUpdateInterval(2);
+    m_mouseUp->setEndFrame(255);
+    m_mouseUp->setEasingCurve(QEasingCurve::OutCubic);
+    connect(m_mouseUp, &QTimeLine::frameChanged,
+            this, &KNStdLibViewerCategoryButton::onActionMousePressedChange);
 }
 
 void KNStdLibViewerCategoryButton::resizeEvent(QResizeEvent *event)
 {
     QWidget::resizeEvent(event);
+    m_iconCenterX=(width()-m_iconSize)>>1;
+    update();
 }
 
 void KNStdLibViewerCategoryButton::enterEvent(QEvent *event)
@@ -34,6 +72,9 @@ void KNStdLibViewerCategoryButton::enterEvent(QEvent *event)
     if(!m_checked)
     {
         //Show Mouse Enter State
+        m_mouseOut->stop();
+        m_mouseIn->setStartFrame(m_highLightColor.alpha());
+        m_mouseIn->start();
     }
 }
 
@@ -42,7 +83,7 @@ void KNStdLibViewerCategoryButton::leaveEvent(QEvent *event)
     QWidget::leaveEvent(event);
     if(!m_checked)
     {
-        //Show Normal State
+        startMouseOutAnime();
     }
 }
 
@@ -52,6 +93,9 @@ void KNStdLibViewerCategoryButton::mousePressEvent(QMouseEvent *event)
     if(!m_checked)
     {
         //Show Mouse Down State
+        m_mouseUp->stop();
+        m_mouseDown->setStartFrame(m_highLightColor.alpha());
+        m_mouseDown->start();
         m_mousePressed=true;
     }
 }
@@ -68,14 +112,99 @@ void KNStdLibViewerCategoryButton::mouseReleaseEvent(QMouseEvent *event)
     KNLibViewerCategoryButton::mouseReleaseEvent(event);
     if(!m_checked)
     {
-        //
-        ;
+        //Check if the mouse has been pressed before, avoid other released.
+        if(m_mousePressed)
+        {
+            startMouseUpAnime();
+            emit clicked();
+        }
     }
 }
 
 void KNStdLibViewerCategoryButton::paintEvent(QPaintEvent *event)
 {
-    ;
+    Q_UNUSED(event);
+    QPainter painter(this);
+    painter.setPen(Qt::NoPen);
+    //Draw background
+    painter.setBrush(m_background);
+    painter.drawRect(rect());
+    //Draw top line
+    painter.setBrush(m_highLightColor);
+    painter.drawRect(0,0,width(),2);
+    //Draw icon
+    painter.setOpacity(m_iconAlpha);
+    painter.drawPixmap(m_iconCenterX,
+                       m_iconY,
+                       m_iconSize,
+                       m_iconSize,
+                       m_icon);
+    //Draw text
+    painter.setPen(QColor(255,255,255));
+    painter.setOpacity(m_textAlpha);
+    painter.drawText(0,m_textY,width(), height(),
+                     Qt::AlignHCenter,
+                     m_text);
+}
+
+void KNStdLibViewerCategoryButton::onCheckedChanged()
+{
+    if(m_checked)
+    {
+        m_textAlpha=1.0;
+        m_iconAlpha=0.0;
+        m_textY=7;
+        startMouseUpAnime();
+    }
+    else
+    {
+        startMouseOutAnime();
+    }
+}
+
+void KNStdLibViewerCategoryButton::onActionMouseInOutFrameChange(const int &frame)
+{
+    m_highLightColor.setAlpha(frame);
+    m_gradientColor.setAlpha(frame>>1);
+    m_background.setColorAt(0, m_gradientColor);
+    m_textAlpha=(qreal)frame/100;
+    m_iconAlpha=1.0-m_textAlpha;
+    m_textY=-93+frame;
+    update();
+}
+
+void KNStdLibViewerCategoryButton::onActionMousePressedChange(const int &frame)
+{
+    m_highLightColor.setAlpha(frame);
+    m_gradientColor.setAlpha(frame>>1);
+    m_background.setColorAt(0, m_gradientColor);
+    update();
+}
+
+void KNStdLibViewerCategoryButton::startMouseOutAnime()
+{
+    //Show Normal State
+    m_mouseDown->stop();
+    m_mouseUp->stop();
+    m_mouseIn->stop();
+    if(m_mouseOut->state()!=QTimeLine::Running)
+    {
+        m_mouseOut->setStartFrame(m_highLightColor.alpha());
+        m_mouseOut->start();
+    }
+}
+
+void KNStdLibViewerCategoryButton::startMouseUpAnime()
+{
+    //Stop all other animations.
+    m_mouseOut->stop();
+    m_mouseIn->stop();
+    m_mouseDown->stop();
+    if(m_mouseUp->state()!=QTimeLine::Running)
+    {
+        m_mouseUp->setStartFrame(m_highLightColor.alpha());
+        m_mouseUp->start();
+    }
 }
 
 QString KNStdLibViewerCategoryButton::text() const
@@ -85,7 +214,7 @@ QString KNStdLibViewerCategoryButton::text() const
 
 void KNStdLibViewerCategoryButton::setText(const QString &text)
 {
-    m_text = text;
+    m_text=text;
 }
 
 QPixmap KNStdLibViewerCategoryButton::icon() const
@@ -95,6 +224,8 @@ QPixmap KNStdLibViewerCategoryButton::icon() const
 
 void KNStdLibViewerCategoryButton::setIcon(const QPixmap &icon)
 {
-    m_icon = icon;
+    m_icon=icon.scaled(m_iconSize,m_iconSize,
+                       Qt::IgnoreAspectRatio,
+                       Qt::SmoothTransformation);
 }
 
--- src/Modules/Base/knstdlibviewercategorybutton.h
+++ src/Modules/Base/knstdlibviewercategorybutton.h
@@ -1,6 +1,8 @@
 #ifndef KNSTDLIBVIEWERCATEGORYBUTTON_H
 #define KNSTDLIBVIEWERCATEGORYBUTTON_H
 
+#include <QLinearGradient>
+
 #include "knlibviewercategorybutton.h"
 
 class QBoxLayout;
@@ -30,12 +32,23 @@ protected:
     void mouseMoveEvent(QMouseEvent *event);
     void mouseReleaseEvent(QMouseEvent *event);
     void paintEvent(QPaintEvent *event);
+    void onCheckedChanged();
 
 private slots:
+    void onActionMouseInOutFrameChange(const int &frame);
+    void onActionMousePressedChange(const int &frame);
 
 private:
+    void startMouseOutAnime();
+    void startMouseUpAnime();
     QPixmap m_icon;
     QString m_text;
+    QLinearGradient m_background;
+    int m_iconSize=20, m_fixedHeight=30, m_iconY=5, m_iconCenterX=0, m_textY=-95;
+    qreal m_iconAlpha=1.0, m_textAlpha=0.0;
+    QColor m_highLightColor=QColor(255,255,255,0),
+           m_gradientColor=QColor(255,255,255,0);
+    QTimeLine *m_mouseIn, *m_mouseOut, *m_mouseUp, *m_mouseDown;
     bool m_mousePressed=false;
 };
 
